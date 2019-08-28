e90eaae399e0755b9f16fed7024ad997d3ae1dac
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-28 17:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file!
 const feetPerBlock = 264;
+const hqBlock = 42;
 
 function distanceFromHqInBlocks(block) {
-  let result = Math.ceil(feet/feetPerBlock);
+  let result = block - hqBlock;
   return result;
 }
